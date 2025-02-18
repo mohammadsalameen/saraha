c64467e7adc27de3e16a068d30e8d54ea0b332e6
mohammadsalamin <msalameen.123456@gmail.com>
2025-02-18 18:16:34 +0200

    add a unknown and welcome pages

--- index.router.js
+++ index.router.js
@@ -2,10 +2,15 @@ import { connectDB } from "./DB/connection.js"
 import userRouter from './src/modules/user/user.router.js'
 import authRouter from './src/modules/authentication/auth.router.js'
 import messageRouter from './src/modules/message/message.router.js'
+import { AppError } from "./src/utils/AppError.js"
 export const initApp = (app, express) =>{
     connectDB();
     app.use(express.json());
     app.use('/users',userRouter);
     app.use('/auth',authRouter);
     app.use('/message', messageRouter);
+    app.use('/', (req, res) => res.status(200).json("Welcome"));
+    app.use('*', (req, res, next) => {
+        return next(new AppError("Page not found", 404))
+    });
 }
